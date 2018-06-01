1b5ab1080ee153404e05e06b06e7a474b9c58938
david <david.journo.31@gmail.com>
2018-06-01 14:55:42 +0200

    location and distance innerHTML

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-var temperatureContainer = document.getElementById('temperature');
+// API OPENWEATHER
 var apiWeather = "https://api.openweathermap.org/data/2.5/weather?id=2972315&appid=a7d9a2f5fac5c4d3101c5e68f3fae892&units=metric";
-var apiAdress = "https://api.openweathermap.org/data/2.5/weather?q=Saint-Gaudens,fr&appid=a7d9a2f5fac5c4d3101c5e68f3fae892&units=metric";
-;
-var apiLocation = "https://";
+// API GOOGLE MAP
+var apiLocation = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=Toulouse,FR&destinations=Saint-Gaudens,FR&key=AIzaSyDCdXmgAuBOf53mKmSLDfyMm0ZWcc05H_Y";
 
+// METEO
 var btn = document.getElementById('btn');
+var temperatureContainer = document.getElementById('temperature');
 
+// LOCALISATION NON UTILISE
+// var btnLoc = document.getElementById('btn-adress');
+// var locContainer = document.getElementById('adresse');
+
+// ITINERAIRE
+var btnDistance = document.getElementById('btn-itin');
+var distContainer = document.getElementById('itineraire');
+
+// EVENT CLICK METEO & LOCALISATION
 btn.addEventListener('click', function () {
     var request = new XMLHttpRequest();
 
@@ -19,9 +29,15 @@ btn.addEventListener('click', function () {
 
             var tempC = dataWeather.main.temp + "°C" + "<br>";
             var description = dataWeather.weather[0].description;
-            temperatureContainer.innerHTML = "Temperature Toulouse : " + tempC + "<br>" + "Description : " + description;
+            var lon = dataWeather.coord.lon;
+            console.log(lon);
+            var lat = dataWeather.coord.lat;
+            console.log(lat);
 
-            // temperatureContainer.insertAdjacentHTML('beforeend', "Temperature Toulouse : " + tempC);
+            
+            var format = "<br>" + "Temperature Toulouse : " + tempC + "<br>" + "Description : " + description + "<br>" + "<h1>" + "Coordonnées" + "</h1>" + "Longitude : " + lon + "<br>" + "Latitude : " + lat;
+
+            temperatureContainer.innerHTML = format;
         } else {
             console.log("connected but error");
         }
@@ -30,8 +46,26 @@ btn.addEventListener('click', function () {
     request.send();
 });
 
-// function addHtml(temp) {
-//     console.log("test function");
-//     tempContainer.insertAdjacentHTML('beforeend', "temperature Toulouse :" +temp.main.temp + "°C");
-// }
+
+
+// EVENT CLICK DISTANCE
+// btnDistance.addEventListener('click', function(){
+//     var request = new XMLHttpRequest();
+
+//     request.open('GET', apiLocation);
+//     request.onload = function () {
+//         var dataLocation = JSON.parse(request.responseText);
+//         console.log(dataLocation);
+
+//         var distance = dataLocation.rows[0].elements[0].distance.text;
+//         var duration = dataLocation.rows[0].elements.duration.text;
+//         console.log(distance);
+//         console.log(duration);
+//         var format = "<br>" + "Distance : " + distance + "KM" + "<br>" + "Durée : " + duration;
+
+//         distContainer.innerHTML = format;
+
+//     };
+//     request.send();
+// });
 
